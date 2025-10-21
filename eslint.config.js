a14e73dcdadd066a2fcf92fa5b0c5239e392f30f
Johann Jungbauer <jungbauer@gmail.com>
2025-10-21 17:07:04 +0200

    chore: adds prettier config into eslint

--- eslint.config.js
+++ eslint.config.js
@@ -2,8 +2,15 @@ import js from "@eslint/js";
 import globals from "globals";
 import pluginVue from "eslint-plugin-vue";
 import { defineConfig } from "eslint/config";
+import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended";
 
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs,vue}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.browser } },
+  {
+    files: ["**/*.{js,mjs,cjs,vue}"],
+    plugins: { js },
+    extends: ["js/recommended"],
+    languageOptions: { globals: globals.browser },
+  },
   pluginVue.configs["flat/essential"],
+  eslintPluginPrettierRecommended,
 ]);
