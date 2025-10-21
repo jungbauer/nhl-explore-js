86bfa5649616c376d4a2f66609472a0861c0db00
Johann Jungbauer <jungbauer@gmail.com>
2025-10-21 17:25:08 +0200

    fix: removes old commands from lint

--- eslint.config.js
+++ eslint.config.js
@@ -3,8 +3,13 @@ import globals from "globals";
 import pluginVue from "eslint-plugin-vue";
 import { defineConfig } from "eslint/config";
 import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended";
+import { includeIgnoreFile } from "@eslint/compat";
+import { fileURLToPath } from "node:url";
+
+const gitignorePath = fileURLToPath(new URL(".gitignore", import.meta.url));
 
 export default defineConfig([
+  includeIgnoreFile(gitignorePath, "Imported .gitignore patterns"),
   {
     files: ["**/*.{js,mjs,cjs,vue}"],
     plugins: { js },
