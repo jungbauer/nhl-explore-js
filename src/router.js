902ab27cfed5361f846244c2dba14cec60116e64
Johann Jungbauer <jungbauer@gmail.com>
2025-10-25 12:43:07 +0200

    chore: removes log

--- src/router.js
+++ src/router.js
@@ -5,8 +5,6 @@ import GameView from "@/components/GameView.vue";
 
 const baseUrl = "/nhl-explore-js";
 
-console.log("router test", import.meta.env.BASE_URL);
-
 const routes = [
   { path: `${baseUrl}/`, component: StandingsView },
   { path: `${baseUrl}/schedule/:team`, component: TeamScheduleView },
