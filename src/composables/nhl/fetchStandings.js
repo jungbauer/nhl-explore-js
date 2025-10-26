6c26be3e35fdf52f098fd9b6fdbb783859bba1a1
Johann Jungbauer <jungbauer@gmail.com>
2025-10-26 13:15:05 +0200

    build: add env variable for api

--- src/composables/nhl/fetchStandings.js
+++ src/composables/nhl/fetchStandings.js
@@ -1,15 +1,7 @@
 import useFetch from "@/composables/useFetch.js";
 
 const fetchStandings = () => {
-  const getDate = () => {
-    const now = new Date();
-    const year = now.getFullYear();
-    const month = (now.getMonth() + 1).toString().padStart(2, "0");
-    const day = now.getDate().toString().padStart(2, "0");
-    return `${year}-${month}-${day}`; // formatted date
-  };
-
-  const standingsUrl = `/api/standings/${getDate()}`;
+  const standingsUrl = `${import.meta.env.VITE_API_URL}/nhl/standings`;
   const [startFetch] = useFetch(standingsUrl);
 
   const startFetchStandings = async () => {
